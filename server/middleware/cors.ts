4ea9afb27d297c8b1377612510b5c9b91b90768e
Дмитрий Василевский <dmitry.vasilevsky@immelman.ru>
2024-03-21 17:32:13 +0300

    fix cors

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -1,33 +1,30 @@
-const ORIGINS = [
-  "divasilevski.github.io",
-  "master--melodious-marigold-1931f5.netlify.app",
-];
-
 /**
  * Middleware to respond CORS preflight requests using OPTIONS method.
  */
 export default defineEventHandler((event) => {
   const host = event.node.req.headers.host;
 
-  setResponseHeaders(event, {
-    "Access-Control-Allow-Origin": "https://divasilevski.github.io",
-    "Access-Control-Allow-Methods": "*",
-    "Access-Control-Allow-Credentials": "true",
-    "Access-Control-Allow-Headers": "https://divasilevski.github.io",
-    "Access-Control-Expose-Headers": "*",
-  });
+  if (host?.includes("divasilevski.github.io")) {
+    setResponseHeaders(event, {
+      "Access-Control-Allow-Origin": "https://divasilevski.github.io",
+      "Access-Control-Allow-Methods": "*",
+      "Access-Control-Allow-Credentials": "true",
+      "Access-Control-Allow-Headers": "https://divasilevski.github.io",
+      "Access-Control-Expose-Headers": "*",
+    });
+  }
 
-  // ORIGINS.forEach((origin) => {
-  //   if (host?.startsWith(origin)) {
-  //     setResponseHeaders(event, {
-  //       "Access-Control-Allow-Origin": "https://" + origin,
-  //       "Access-Control-Allow-Methods": "*",
-  //       "Access-Control-Allow-Credentials": "true",
-  //       "Access-Control-Allow-Headers": "https://" + origin,
-  //       "Access-Control-Expose-Headers": "*",
-  //     });
-  //   }
-  // });
+  if (host?.includes("master--melodious-marigold")) {
+    setResponseHeaders(event, {
+      "Access-Control-Allow-Origin":
+        "https://master--melodious-marigold-1931f5.netlify.app",
+      "Access-Control-Allow-Methods": "*",
+      "Access-Control-Allow-Credentials": "true",
+      "Access-Control-Allow-Headers":
+        "https://master--melodious-marigold-1931f5.netlify.app",
+      "Access-Control-Expose-Headers": "*",
+    });
+  }
 
   // Answers HTTP 204 OK to CORS preflight requests using OPTIONS method :
   // if (event.method === 'OPTIONS' && isPreflightRequest(event)) {
