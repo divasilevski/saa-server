afa61ddcd11ce560974a043cd75de8bc935bdc96
Дмитрий Василевский <dmitry.vasilevsky@immelman.ru>
2024-03-21 17:18:55 +0300

    Add cors

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -1,7 +1,26 @@
+const ORIGINS = [
+  "https://divasilevski.github.io",
+  "https://master--melodious-marigold-1931f5.netlify.app",
+];
+
 /**
  * Middleware to respond CORS preflight requests using OPTIONS method.
  */
 export default defineEventHandler((event) => {
+  const host = event.node.req.headers.host;
+
+  ORIGINS.forEach((origin) => {
+    if (host?.startsWith(origin)) {
+      setResponseHeaders(event, {
+        "Access-Control-Allow-Origin": origin,
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Allow-Headers": origin,
+        "Access-Control-Expose-Headers": "*",
+      });
+    }
+  });
+
   // Answers HTTP 204 OK to CORS preflight requests using OPTIONS method :
   // if (event.method === 'OPTIONS' && isPreflightRequest(event)) {
   if (isPreflightRequest(event)) {
