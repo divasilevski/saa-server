6572f91b284a96c55834174a3c62128406c45378
Дмитрий Василевский <dmitry.vasilevsky@immelman.ru>
2024-03-25 10:31:02 +0300

    add status for message

--- app.vue
+++ app.vue
@@ -8,72 +8,63 @@
 <script lang="ts" setup>
 const data = ref()
 
-function doThingsWithCookies () {
+const message = (data: object) => {
+  // window.parent.postMessage(document.cookie, "*");
+  window.parent.postMessage(JSON.stringify(data), "*");
+}
+
+function doThingsWithCookies (status: string) {
   data.value = document.cookie;
+  message({ event: 'SAA', cookie: document.cookie, status })
 }
 
 async function handleCookieAccess () {
   if (!document.hasStorageAccess) {
     // This browser doesn't support the Storage Access API
     // so let's just hope we have access!
-    doThingsWithCookies();
+    doThingsWithCookies('not-supported');
   } else {
     const hasAccess = await document.hasStorageAccess();
     if (hasAccess) {
       // We have access to third-party cookies, so let's go
-      doThingsWithCookies();
+      doThingsWithCookies('granted');
     } else {
       // Check whether third-party cookie access has been granted
       // to another same-site embed
       try {
         const permission = await navigator.permissions.query({
-          name: "storage-access",
+          name: "storage-access" as PermissionName,
         });
 
         if (permission.state === "granted") {
           // If so, you can just call requestStorageAccess() without a user interaction,
           // and it will resolve automatically.
           await document.requestStorageAccess();
-          doThingsWithCookies();
+          doThingsWithCookies('granted');
         } else if (permission.state === "prompt") {
+          doThingsWithCookies("prompt");
           // Need to call requestStorageAccess() after a user interaction
-          const $btn = document.querySelector('#btn')
-          $btn.addEventListener("click", async () => {
-            try {
-              await document.requestStorageAccess();
-              doThingsWithCookies();
-            } catch (err) {
-              // If there is an error obtaining storage access.
-              console.error(`Error obtaining storage access: ${err}.
-                            Please sign in.`);
-            }
-          });
+          // const $btn = document.querySelector('#btn')
+          // $btn.addEventListener("click", async () => {
+          //   try {
+          //     await document.requestStorageAccess();
+          //     doThingsWithCookies();
+          //   } catch (err) {
+          // If there is an error obtaining storage access.
+          //     console.error(`Error obtaining storage access: ${err}.
+          //                   Please sign in.`);
+          //   }
+          // });
         } else if (permission.state === "denied") {
-          // User has denied third-party cookie access, so we'll
-          // need to do something else
+          doThingsWithCookies('denied')
         }
       } catch (error) {
         console.log(`Could not access permission state. Error: ${error}`);
-        doThingsWithCookies(); // Again, we'll have to hope we have access!
+        doThingsWithCookies('denied'); // Again, we'll have to hope we have access!
       }
     }
   }
 }
-
-// $btn.onclick = async () => {
-// Safari variant
-// await document.requestStorageAccess();
-
-// if (await document.hasStorageAccess()) {
-//   window.parent.postMessage(document.cookie, "*");
-// }
-
-// Firefox
-// if (await hasCookieAccess()) {
-//   window.parent.postMessage(document.cookie, "*");
-// }
-
-// };
 
 onMounted(() => {
   handleCookieAccess();
